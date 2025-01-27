f09528fed139813cd820bdb34e393ac9623a9830
Nate Bross <nate@brosstribe.com>
2025-01-27 17:58:53 -0600

    fix: more coverage

--- tests/MotionExtract.Tests/MotionPhotoShould.cs
+++ tests/MotionExtract.Tests/MotionPhotoShould.cs
@@ -57,4 +57,34 @@ public class MotionPhotoShould
         // Assert
         Assert.Equal(24, photoVideoBase.Mp4Data[0].Length);
     }
+
+    [Fact]
+    public void Should_Have_Zero_Data_Blocks()
+    {
+        // Arrange
+        var baseFile = new FileInfo("SyntheticFiles/OneV.jpg");
+        var photoVideoBase = new MotionPhoto(baseFile);
+
+        // Act
+        photoVideoBase.Extract();
+
+        // Assert
+        Assert.Empty(photoVideoBase.JpgData);
+        Assert.Empty(photoVideoBase.Mp4Data);
+    }
+
+    [Fact]
+    public void Should_Have_Zero_Data_Blocks_For_Non_Motion_Photo()
+    {
+        // Arrange
+        var baseFile = new FileInfo("SyntheticFiles/EmptyFile.jpg");
+        var photoVideoBase = new MotionPhoto(baseFile);
+
+        // Act
+        photoVideoBase.Extract();
+
+        // Assert
+        Assert.Empty(photoVideoBase.JpgData);
+        Assert.Empty(photoVideoBase.Mp4Data);
+    }
 }
